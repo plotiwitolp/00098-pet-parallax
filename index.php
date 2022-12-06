2675e031df8c756f2ed4c0365c8cf0f509c6a2f4
plotiwitolp <plotiwitolp@gmail.com>
2022-12-06 19:36:34 +0400

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>00098-pet</title>
+  <link rel="stylesheet" href="./style.css" />
+</head>
+
+<body>
+  <div class="scene">
+    <div data-offset="90" class="parallax clouds"></div>
+    <div data-offset="50" class="parallax buildings"></div>
+    <div data-offset="30" class="parallax trees"></div>
+    <div data-offset="25" class="parallax cat"></div>
+    <div data-offset="20" class="parallax grass"></div>
+    <div class="ground"></div>
+  </div>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="./script.js"></script>
+</body>
+
+</html>
